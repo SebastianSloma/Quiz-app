1b476301d52de88020b58eba0c1ca4d005e0f101
Sebastian <sebastiansloma@gmail.com>
2023-01-04 15:44:10 +0100

    added communications with page

--- app.py
+++ app.py
@@ -5,15 +5,23 @@ from flask import render_template, redirect, request, url_for, flash
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'VeryHardKey2Gu3es!'
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 @app.route('/quiz1')
-def quiz():
+def quiz1():
     return render_template('quiz1.html')
+
 
 @app.route('/quiz1_result')
 def quiz1_result():
@@ -24,13 +32,16 @@ def quiz1_result():
 def quiz2():
     return render_template('quiz2.html')
 
+
 @app.route('/quiz2_result')
 def quiz2_result():
     return render_template('quiz2_result.html')
 
+
 @app.route('/quiz3')
 def quiz3():
     return render_template('quiz3.html')
+
 
 @app.route('/quiz3_result')
 def quiz3_result():
@@ -40,6 +51,7 @@ def quiz3_result():
 @app.route('/quiz4')
 def quiz4():
     return render_template('quiz4.html')
+
 
 @app.route('/quiz4_result')
 def quiz4_result():
