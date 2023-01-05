2e808d5827e9fbb6e0dde20f873b156940adeb0b
Sebastian <sebastiansloma@gmail.com>
2023-01-05 17:17:01 +0100

    added questions for first and second quizzes

--- app.py
+++ app.py
@@ -7,6 +7,157 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'VeryHardKey2Gu3es!'
 
+quiz_no1 = [{
+            'question': 'What is a correct syntax to output "Hello World" in Python?',
+            'answer': ['echo("Hello Wolrd")', 'print("Hello World")', 'p("Hello World")'],
+            'correct': 'print("Hello World")'},
+            {
+            'question': 'Which is a valid variable name in Python?',
+            'answer': ['1_data', 'old-data', '_output'],
+            'correct': '_output'},
+            {
+            'question': 'How do you insert COMMENTS in Python code?',
+            'answer': ['#This is a comment', '//This is a comment', '/*This is a comment*/'],
+            'correct': '#This is a comment'},
+            {
+            'question': 'How do you create a variable with the numeric value 5?',
+            'answer': ['x=5', 'x=int(5)', 'Both answers are correct'],
+            'correct': 'Both answers are correct'},
+            {
+            'question': 'What is the correct file extension for Python files?',
+            'answer': ['.py', '.pyt', '.pt'],
+            'correct': '.py'},
+            {
+            'question': 'How do you create a variable with the floating number 2.8',
+            'answer': ['x=2.8', 'x=float(2.8)', 'Both answers are correct'],
+            'correct': 'Both answers are correct'},
+            {
+            'question': 'What is the correct syntax to output the type of a variable or object in Python?',
+            'answer': ['print(typeOf(x))', 'print(typeof x)', 'print(type(x))'],
+            'correct': 'print(type(x))'},
+            {
+            'question': 'What is the correct way to create a function in Python?',
+            'answer': ['function myFunction():', 'def myFunction():', 'create myFunction():'],
+            'correct': 'def myFunction():'},
+            {
+            'question': 'What is a correct syntax to return the first character in a string?',
+            'answer': ['x = "Hello".sub(0, 1)', 'x = sub("Hello", 0, 1)', 'x = sub("Hello")[0]'],
+            'correct': ''}
+            ]
+
+quiz_no2 = [{
+            'question': 'Which method can be used to remove any whitespace from both the beginning and the end of a string?',
+            'answer': ['trim()', 'len()', 'strip()'],
+            'correct': ''},
+            {
+            'question': 'Which method can be used to return a string in upper case letters?',
+            'answer': ['upper()', 'uppercase()', 'touppercase()'],
+            'correct': ''},
+            {
+            'question': 'Which method can be used to replace parts of a string?',
+            'answer': ['repl()', 'replace()', 'switch()'],
+            'correct': ''},
+            {
+            'question': 'Which operator is used to multiply numbers?',
+            'answer': ['%', 'x', '*'],
+            'correct': ''},
+            {
+            'question': 'Which operator can be used to compare two values?',
+            'answer': ['==', '><', '='],
+            'correct': '=='},
+            {
+            'question': 'Which of these collections defines a LIST?',
+            'answer': ['{"name" : "apple", "color" : "red"}', '("apple", "banana", "cherry")', '["apple", "banana", "cherry"]'],
+            'correct': '["apple", "banana", "cherry"]'},
+            {
+            'question': 'Which of these collections defines a TUPLE?',
+            'answer': ['{"apple", "banana", "cherry"}', '("apple", "banana", "cherry")', '["apple", "banana", "cherry"]'],
+            'correct': '("apple", "banana", "cherry")'},
+            {
+            'question': 'Which of these collections defines a SET?',
+            'answer': ['{"apple", "banana", "cherry"}', '("apple", "banana", "cherry")', '["apple", "banana", "cherry"]'],
+            'correct': '{"apple", "banana", "cherry"}'},
+            {
+            'question': 'Which of these collections defines a DICTIONARY?',
+            'answer': ['("apple", "banana", "cherry")', '{"apple", "banana", "cherry"}', '{"name" : "apple", "color" : "red"}'],
+            'correct': '{"name" : "apple", "color" : "red"}'}
+            ]
+
+quiz_no3 = [{
+            'question': '',
+            'answer': ['', '', ''],
+            'correct': ''},
+            {
+            'question': '',
+            'answer': ['', '', ''],
+            'correct': ''},
+            {
+            'question': '',
+            'answer': ['', '', ''],
+            'correct': ''},
+            {
+            'question': '',
+            'answer': ['', '', ''],
+            'correct': ''},
+            {
+            'question': '',
+            'answer': ['', '', ''],
+            'correct': ''},
+            {
+            'question': '',
+            'answer': ['', '', ''],
+            'correct': ''},
+            {
+            'question': '',
+            'answer': ['', '', ''],
+            'correct': ''},
+            {
+            'question': '',
+            'answer': ['', '', ''],
+            'correct': ''},
+            {
+            'question': '',
+            'answer': ['', '', ''],
+            'correct': ''}
+            ]
+
+quiz_no4 = [{
+            'question': '',
+            'answer': ['', '', ''],
+            'correct': ''},
+            {
+            'question': '',
+            'answer': ['', '', ''],
+            'correct': ''},
+            {
+            'question': '',
+            'answer': ['', '', ''],
+            'correct': ''},
+            {
+            'question': '',
+            'answer': ['', '', ''],
+            'correct': ''},
+            {
+            'question': '',
+            'answer': ['', '', ''],
+            'correct': ''},
+            {
+            'question': '',
+            'answer': ['', '', ''],
+            'correct': ''},
+            {
+            'question': '',
+            'answer': ['', '', ''],
+            'correct': ''},
+            {
+            'question': '',
+            'answer': ['', '', ''],
+            'correct': ''},
+            {
+            'question': '',
+            'answer': ['', '', ''],
+            'correct': ''}
+            ]
 
 @app.route('/')
 def index():
